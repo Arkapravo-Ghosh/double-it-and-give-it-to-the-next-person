3e799523fec93f863c6a9cccb7438d64bda0423f
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-03-19 20:10:18 +0530

    Minor Changes

--- prog69.cpp
+++ prog69.cpp
@@ -1,13 +1,9 @@
-#include<iostream>
-
-int main () {
-    
+#include <iostream>
+int main()
+{
     std::cout << "Enter a number: ";
     int num{};
     std::cin >> num;
-
-    //double it and give it to the next person
     std::cout << "Double it and give it to the next person: " << num * 2 << "\n";
-    
     return 0;
 }
