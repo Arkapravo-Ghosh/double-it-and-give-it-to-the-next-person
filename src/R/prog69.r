fea0a59d406b719af54fffe08bc648fe64e2ddf5
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-03-22 19:43:23 +0530

    Fixes made

--- src/R/prog69.r
+++ src/R/prog69.r
@@ -1,13 +1,9 @@
 main <- function() {
-  while (TRUE) {
-    n1 <- readline(prompt="Enter a number: ")
-    if (is.numeric(n1)) {
-      break
-    } else {
-      cat("You must enter an integer. Please try again.\n")
-    }
-  }
-  cat("Double it and give it to the next person: ", n1 * 2, "\n")
+  n1 <- readline(prompt = "Enter a number: ")
+  n1 <- as.numeric(n1)
+  cat("Double it and give it to the next person:", n1 * 2, "\n")
 }
 
-main()
+if (require("methods")) {
+  main()
+}
